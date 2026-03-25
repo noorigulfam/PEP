89416251417664e7e641951e40777f8ce9e34c8b
noorigulfam <153357411+noorigulfam@users.noreply.github.com>
2026-03-25 23:01:53 +0530

    Add files via upload

---
+++ Controllers/ManageDoctorsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PepProject.Models;
+
+namespace PepProject.Controllers
+{
+    public class ManageDoctorsController : Controller
+    {
+
+        public MedDb _db;
+        public ManageDoctorsController() {
+            _db = new MedDb();
+        }
+        public IActionResult DoctorsList()
+        {
+            var res = _db.Doctors.ToList();
+            return View(res);
+        }
+
+        public IActionResult PatientsList()
+        {
+            var res = _db.Patients.ToList();
+            return View(res);
+        }
+
+
+
+        public IActionResult AllAppointments()
+        {
+            var data = (from b in _db.BookingDoctorModel_Appointments
+                        join d in _db.Doctors on b.DoctorID equals d.DoctorId
+                        join p in _db.Patients on b.PatientID equals p.PatientId
+                        select new BookingDoctorModel
+                        {
+                            ID = b.ID,
+                            DoctorName = d.DoctorName,
+                            PatientName = p.PatientName,
+                            Date = b.Date,
+                            Status = b.Status
+                        }).ToList();
+
+            return View(data);
+        }
+
+
+        [HttpGet]
+
+        public IActionResult Approve(int id)
+        {
+            var booking = _db.BookingDoctorModel_Appointments.FirstOrDefault(x => x.ID == id);
+
+            if (booking != null)
+            {
+                booking.Status = "Approved";
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("AllAppointments");
+        }
+
+        public IActionResult Reject(int id)
+        {
+            var booking = _db.BookingDoctorModel_Appointments.FirstOrDefault(x => x.ID == id);
+
+            if (booking != null)
+            {
+                booking.Status = "Rejected";
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("AllAppointments");
+        }
+    }
+}
---
+++ Controllers/ManagePatientsController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PepProject.Models;
+
+namespace PepProject.Controllers
+{
+    public class ManagePatientsController : Controller
+    {
+        public MedDb _db;
+        public ManagePatientsController()
+        {
+            _db = new MedDb();
+        }
+
+        public IActionResult MyPatient()
+        {
+            var res = _db.Patients.ToList();
+            return View(res);
+        }
+
+
+        // 📅 Appointment Calendar Page
+        public IActionResult AppointmentCalendar()
+        {
+            int doctorId = Convert.ToInt32(HttpContext.Session.GetInt32("DoctorId"));
+
+            var data = (from b in _db.BookingDoctorModel_Appointments
+                        join p in _db.Patients on b.PatientID equals p.PatientId
+                        where b.DoctorID == doctorId
+                        select new BookingDoctorModel
+                        {
+                            PatientName = p.PatientName,
+                            Date = b.Date,
+                            Status = b.Status
+                        }).ToList();
+
+            return View(data);
+        }
+
+
+        // ⏰ Manage Availability Page
+        public IActionResult ManageAvailability()
+        {
+            return View();
+        }
+
+        public IActionResult BookAppointment()
+        {
+            var res = _db.Doctors.ToList();
+            return View(res);
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult BookDoctor(int DoctorId)
+        {
+            var model = new BookingDoctorModel()
+            {
+                DoctorID = DoctorId
+
+            };
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public IActionResult BookDoctor(BookingDoctorModel model)
+        {
+
+            // Example: get PatientId from session
+            int patientId = Convert.ToInt32(HttpContext.Session.GetInt32("PatientId"));
+
+            model.PatientID = patientId;
+            model.Status = "Pending";
+
+            _db.BookingDoctorModel_Appointments.Add(model);
+            _db.SaveChanges();
+
+            return RedirectToAction("MyAppointments");
+
+
+        }
+
+
+        public IActionResult MyAppointments()
+        {
+            var data = (from b in _db.BookingDoctorModel_Appointments
+                        join d in _db.Doctors on b.DoctorID equals d.DoctorId
+                        join p in _db.Patients on b.PatientID equals p.PatientId
+                        select new BookingDoctorModel
+                        {
+                            DoctorName = d.DoctorName,
+                            PatientName = p.PatientName,
+                            Date = b.Date,
+                            Status = b.Status
+                        }).ToList();
+            return View(data);
+        }
+
+
+        
+
+
+    }
+}
